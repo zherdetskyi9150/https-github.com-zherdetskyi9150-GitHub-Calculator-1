0ccc39a86def76046a44d1d5869b721107ef83d0
emyca <evmyca@gmail.com>
2022-12-06 22:18:41 +0200

    Add app version

--- src/app/Main.java
+++ src/app/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("My app for Users!");
-
+        System.out.println("Version 1.0");
         int a = 8;
         int b = 2;
         int c = add(a, b);
