25ca548bc0fd20e11c3fc264758bfdb811928c29
emyca <evmyca@gmail.com>
2022-12-06 22:21:20 +0200

    Add app description

--- src/app/Main.java
+++ src/app/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Super app!");
-
+        System.out.println("Calculator for base calculations");
         int a = 8;
         int b = 2;
         int c = add(a, b);
