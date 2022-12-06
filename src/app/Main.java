85599af03f51a184d52eb2d42a2230b804d3f9d8
emyca <evmyca@gmail.com>
2022-12-06 22:15:51 +0200

    Change app info: short text

--- src/app/Main.java
+++ src/app/Main.java
@@ -3,7 +3,7 @@ package app;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("My git app. Super!");
+        System.out.println("Super app!");
 
         int a = 8;
         int b = 2;
