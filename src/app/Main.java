7eda358bd22064717a9a2b75fc06420047d8c4f1
Влад <Port3455k@gmail.com>
2022-12-08 19:26:31 +0200

    add subtraction, division

--- src/app/Main.java
+++ src/app/Main.java
@@ -11,6 +11,10 @@ public class Main {
         System.out.println(c);
         int d = multiply(a, b);
         System.out.println(d);
+        int e = subtraction(a, b);
+        System.out.println(e);
+        int f = division(a, b);
+        System.out.println(f);
     }
 
     private static int add(int a, int b) {
@@ -20,4 +24,9 @@ public class Main {
     private static int multiply(int a, int b) {
         return a * b;
     }
+
+    private static int subtraction(int a, int b) { return a - b; }
+
+    private static int division(int a, int b) { return a / b;}
+
 }
